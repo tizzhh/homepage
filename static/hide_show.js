ec59ebfa8b8fcef291b83fd6784e539827ff2575
tizzhh <darovadraste2@gmail.com>
2022-08-17 22:04:54 +0300

    new .js

---
+++ static/hide_show.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#about").click(function(){
+        $("#indexsec").hide();
+        $("#aboutsec").show();
+    });
+    $("#coding").click(function(){
+        $("#indexsec").hide();
+        $("#codingsec").show();
+    });
+    $("#socials").click(function(){
+        $("#indexsec").hide();
+        $("#socialssec").show();
+    });
+    $("#projects").click(function(){
+        $("#indexsec").hide();
+        $("#projectssec").show();
+    });
+    $("#backtoindex, #backtoindex1, #backtoindex2, #backtoindex3").click(function(){
+        $("#aboutsec").hide();
+        $("#codingsec").hide();
+        $("#socialssec").hide();
+        $("#projectssec").hide()
+        $("#indexsec").show();
+    });
+});
